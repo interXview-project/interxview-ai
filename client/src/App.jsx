ebbb7d0c55be6880ced187fd61832c8dc09f2421
mohammedzyoud <mohammedzyoud25@gmail.com>
2025-11-15 14:43:19 +0300

    Commit for new branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login/Login";
+import SignUp from "./components/Login/SignUp";
+import Footer from "./components/footer/Footer"; 
+
+// Layout component
+function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-grow">{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div className="bg-green-600 text-white p-10 text-4xl font-bold">
-      Tailwind is working now 🎉💚
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/signup"
+          element={
+            <Layout>
+              <SignUp />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
