1545d0c2eb20fd11981a88d48988fbc4be10e0dc
HammamAlnababteh <hammamnababteh098@gmail.com>
2025-11-18 16:31:04 +0300

    Connect Login UI to backend + toast alerts for login page

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import { testDBConnection } from "./config/db.js";
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -22,7 +25,7 @@ app.get("/test", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
