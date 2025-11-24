ca53793ff8438dff30494170d8141f34febbf318
Baraahadi <baraahadi70@gmail.com>
2025-11-24 19:34:11 +0300

    {completed merged all branches}

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
 import Home from "./pages/Home.jsx";
-import CVAnalyzer from "./pages/CvAnalyzer.jsx";
+// import CVAnalyzer from "./pages/CvAnalyzer.jsx";
 import Login from "./pages/LoginScreen.jsx";
+import SignUp from "./pages/SignupScreen.jsx";
+import Interview from "./pages/Interview.jsx";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 import MockTest from "./pages/MockTest.jsx";
 
 export default function App() {
+  const routes = [
+    { name: "Home", path: "/" },
+    { name: "Interview", path: "/interview" }, // ← أضفناه
+
+    { name: "Mock Test", path: "/mock-test" },
+    // { name: "CV Analyzer", path: "/cv-analyzer" },
+  ];
+
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar />
+        <Navbar routes={routes} />
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/mock-test" element={<MockTest />} />
+          <Route path="/interview" element={<Interview />} />
 
-          <Route path="/cv-analyzer" element={<CVAnalyzer />} />
+          <Route path="/mock-test" element={<MockTest />} />
+          {/* <Route path="/cv-analyzer" element={<CVAnalyzer />} /> */}
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
