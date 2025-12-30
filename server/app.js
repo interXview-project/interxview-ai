81595e8bff2253d0049ea3c9ee3fc73f83856c09
Baraahadi <baraahadi70@gmail.com>
2025-12-30 09:29:44 +0300

    {check backend server connection}

--- server/app.js
+++ server/app.js
@@ -24,6 +24,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
